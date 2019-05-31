e3908f0b865a40c702dc285d12e61cca2d8e1908
josefias <joaopereira03@gmail.com>
2019-05-31 22:07:38 +0100

    drone

--- socketTest.py
+++ socketTest.py
@@ -1,13 +1,15 @@
 import socket
 import pickle
 import time
+from pyparrot.Minidrone import Mambo
 
 # Create a socket object 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		 
 
-# Define the port on which you want to connect 
+# GLOBAL 
 port = 8881
 msg = "nada"
+droneList = []
 
 s.connect(("10.0.6.163", port)) # IP UBI
 #s.connect(("0.0.0.0", port))    # IP casa
@@ -19,16 +21,21 @@ while True:
         time.sleep(0.5)
         msg = str(msg)
         print("recebido -%s-" % msg)
-        if(msg != "abort" or msg != ['']):
+        if(msg != "abort"):
 		msg = msg.split(",")
 		if( len(msg) < 2 ):
 			print("is a string \"%s\"" % msg)
                 	#CODIGO PARA CLIENTE QUE APENAS OPERA 1 DRONE
-                	#
-                	#
+                	mambo = Mambo(msg , use_wifi=False)
+			success = mambo.connect(num_retries=2)
+			if(success):
+				mambo.takeoff()
+				mambo.smart_sleep(1)
+				mambo.land()
+                	
 		else:
 			print("is a list ||%s ||| %s|| " % (msg[0],msg[1]) )
-
+			
         else:
                 print("there are no drones.\n you can rest now." )
                 s.close()
