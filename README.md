ad0cfa7a448fa2690fc6c1a6e76bf6f24c7d9a23
josefias <joaopereira03@gmail.com>
2019-06-30 23:25:48 +0100

    info sobre fotos e BLE

--- README.md
+++ README.md
@@ -13,3 +13,18 @@
 3-(não deu) re-install xrdp and vncserver
 		-sudo apt-get remove xrdp vnc4server thightvncserver		
 		-sudo apt-get install xrdp vnc4server thightvncserver
+
+# BLE
+
+### parrot developer statement
+
+5 ARStream protocol
+The ARStream library is designed to send and receive arbitraty binary streams
+using ARNetwork as its network back-end. It is used to transport live audio
+and video data between the product and the controller.
+The ARStream library used an acknowledge system on Bebop Drone
+firmwares before 2.0.17, while the Jumping Sumo never used them. This
+feature won’t be used on newer firmwares (for all products), so implementing it is optionnal for an ARStream compatible library.
+# The ARStream library is not designed to be used on BLE networks.
+(retirado do ARSDK_protocol.pdf)
+
--- socketTest.py
+++ socketTest.py
@@ -1,74 +1,59 @@
-### grande Castro
+# grande Castro
 import socket
 import pickle
 import time
 from pyparrot.Minidrone import Mambo
 
-# Create a socket object 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		 
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# GLOBAL 
+# GLOBAL
 port = 8881
 msg = "nada"
 droneList = []
 t = 0
 
-s.connect(("10.0.6.163", port)) # IP UBI
-#s.connect(("0.0.0.0", port))    # IP casa
+s.connect(("10.0.6.163", port))  # IP UBI
+# s.connect(("0.0.0.0", port))    # IP casa
 print("conected!!")
 
-# connect to the server on local computer 
+# connect to the server on local computer
 while True:
-        msg = s.recv(1024)
-        time.sleep(0.5)
-        msg = str(msg)
-        print("recebido -%s-" % msg)
-        if(msg != "abort"):
-		msg = msg.split(",")
-		if( len(msg) < 2 ):
-			print(type(msg[0]))
-                	#CODIGO PARA CLIENTE QUE APENAS OPERA 1 DRONE
-                	mambo = Mambo(msg[0] , use_wifi=False)
-			success = mambo.connect(num_retries=5)
-			print(success)
-			if(success):
-				mambo.takeoff()
-				mambo.smart_sleep(1)
-				mambo.land()
-                	
-		else:
-			print("is a list ||%s ||| %s|| " % (msg[0],msg[1]) )
-			mambo1 = Mambo(msg[0] , use_wifi=False)
-			mambo2 = Mambo(msg[1] , use_wifi=False)
-			success = mambo1.connect(num_retries=5)
-			print(success)
-			if(success):
-				success = mambo2.connect(num_retries=5)
-				print(success)
-			if(success):
-				mambo1.takeoff()
-				mambo2.takeoff()
-				mambo1.smart_sleep(1)
-				while t < 5:
-					if(t % 2 == 0):
-						print("go up")
-					 	mambo1.fly_direct(roll=0,pitch=0,yaw=0,vertical_movement=30,duration=1)
-						mambo1.smart_sleep(1)
-					 	mambo2.fly_direct(roll=0,pitch=0,yaw=0,vertical_movement=30,duration=1)
-						mambo2.smart_sleep(1)
-					else:
-						print("go down")
-						mambo1.fly_direct(roll=0,pitch=0,yaw=0,vertical_movement=-30,duration=1)
-						mambo1.smart_sleep(1)
-					 	mambo2.fly_direct(roll=0,pitch=0,yaw=0,vertical_movement=-30,duration=1)
-						mambo2.smart_sleep(1)
-					t = t + 1			
-				mambo2.smart_sleep(1)
-				mambo1.land()
-				mambo2.land()			
+    msg = s.recv(1024)
+    time.sleep(0.5)
+    msg = str(msg)
+    print("recebido -%s-" % msg)
+    if(msg != "abort"):
+        msg = msg.split(",")
+        if(len(msg) < 2):
+            print(type(msg[0]))  # CODIGO PARA CLIENTE QUE APENAS OPERA 1 DRONE
+            mambo = Mambo(msg[0], use_wifi=False)
+            success = mambo.connect(num_retries=5)
+            print(success)
+            if(success):
+                mambo.takeoff()
+                mambo.smart_sleep(1)
+                mambo.land()
+
+            else:
+                print("is a list ||%s ||| %s|| " % (msg[0], msg[1]))
+                mambo1 = Mambo(msg[0], use_wifi=False)
+                mambo2 = Mambo(msg[1], use_wifi=False)
+                success = mambo1.connect(num_retries=5)
+                print(success)
+                if(success):
+                    success = mambo2.connect(num_retries=5)
+                    print(success)
+                if(success):
+                    mambo1.takeoff()
+                    mambo2.takeoff()
+                    mambo1.smart_sleep(1)
+                    mambo2.smart_sleep(1)
+                    mambo1.land()
+                    mambo2.land()
         else:
-                print("there are no drones.\n you can rest now." )
-                s.close()
-                quit()
+            print("there are no drones.\n you can rest now.")
+            s.close()
+            quit()
 # close the connection
-s.close()	 
+s.close()
