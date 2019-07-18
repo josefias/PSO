2fb0a6c14ae260173b1e8f04ed82457ebdd0398f
josefias <joaopereira03@gmail.com>
2019-07-18 13:17:25 -0900

    seq. de levantamento de voo seincronizada

--- piServer.py
+++ piServer.py
@@ -15,7 +15,6 @@ def sendData(cli , data):
     msg = pickle.dumps(data) #codificar a data para bytes
     #msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg #
     c.sendall(msg)
-    time.sleep(0.5)
 
 
 ### codigo necessario ao server
@@ -32,28 +31,29 @@ print("waiting for clients...")
 piSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 piSocket.bind(('', 8880))
 #piSocket.bind(('', 8881))
+tempo = time.time()
+timeLeft = 60
+print(tempo)
 
 while True:
-	piSocket.listen(5)
-	c = piSocket.accept()
+    piSocket.listen(5)
+    c = piSocket.accept()
+    print(f"Connection from {c} has been established.")
 
-	print(f"Connection from {c} has been established.")
-
-
-	# drones para os Clientes
-	if(droneNum >= 1):
-    		data = mamboAddr[0]
-    		sendData(c,data)
-    		mamboAddr.remove(mamboAddr[0])
-    		droneNum = len (mamboAddr)
-	else:
-		print("NO MORE DRONES!")  
-		data = "abort"
-		sendData(c,data)
-		#PF: atenção estou a carregar de novo a lista mas 
-		#alguns endereços podem ainda estar em utilização...
-		fh = open("drones.txt")
-		mamboAddr = fh.readlines()
-		droneNum = len(mamboAddr)
-		print (mamboAddr)
-		fh.close()
+    # drones para os Clientes
+    if(droneNum >= 1):
+            data = ( mamboAddr[0], (tempo + 60) )
+            sendData(c,data)
+            mamboAddr.remove(mamboAddr[0])
+            droneNum = len (mamboAddr)
+    else:
+        print("NO MORE DRONES!")  
+        data = ("abort" , (tempo + 1) )
+        sendData(c,data)
+        #PF: atenção estou a carregar de novo a lista mas 
+        #alguns endereços podem ainda estar em utilização...
+        fh = open("drones.txt")
+        mamboAddr = fh.readlines()
+        droneNum = len(mamboAddr)
+        print (mamboAddr)
+        fh.close()
